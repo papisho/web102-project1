e291844860c672b926f717ced7102f66239ed99f
Kidus Bogale <kidusbogale2@gmail.com>
2026-06-10 13:47:35 -0400

    Refactor App component to render multiple TrailCard components from trails data

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import TrailCard from './components/TrailCard'
 import trails from './data/trails'
 
 function App() {
-  const firstTrail = trails[0]
-
   return (
     <div className="app">
       <Header />
-      <TrailCard
-        name={firstTrail.name}
-        location={firstTrail.location}
-        difficulty={firstTrail.difficulty}
-        distance={firstTrail.distance}
-        elevationGain={firstTrail.elevationGain}
-        description={firstTrail.description}
-        image={firstTrail.image}
-        link={firstTrail.link}
-      />
+      <main className="card-grid">
+        {trails.map((trail) => (
+          <TrailCard
+            key={trail.id}
+            name={trail.name}
+            location={trail.location}
+            difficulty={trail.difficulty}
+            distance={trail.distance}
+            elevationGain={trail.elevationGain}
+            description={trail.description}
+            image={trail.image}
+            link={trail.link}
+          />
+        ))}
+      </main>
     </div>
   )
 }
